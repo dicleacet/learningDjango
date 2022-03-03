80803a27b42e9b44db16c997d8dc3d4fa164eeaa
Dicle Acet <71029563+dicleacet@users.noreply.github.com>
2022-03-03 19:32:07 +0300

    add bootstrap

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -1,9 +1,13 @@
-from django.urls import  path
+from django.urls import path
 from . import views
+# http://127.0.0.1:8000/            => homepage
+# http://127.0.0.1:8000/index       => homepage
+# http://127.0.0.1:8000/blogs       => blogs
+# http://127.0.0.1:8000/blogs/3     => blog-details
 
-urlpatterns = [ 
+urlpatterns = [
     path("", views.index),
     path("index", views.index),
     path("blogs", views.blogs),
-    path("blogs/<int:id>", views.blog_details)   
+    path("blogs/<int:id>", views.blog_details),
 ]
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
@@ -8,7 +8,7 @@ def index(request):
 def blogs(request):
     return render(request, "blog/blogs.html")
 
-def blog_details(request,id):
+def blog_details(request, id):
     return render(request, "blog/blog-details.html", {
         "id": id
     })
