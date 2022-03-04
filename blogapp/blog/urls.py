e43c98143cbe5515f555d676c717ea7605b7bc7e
Dicle Acet <71029563+dicleacet@users.noreply.github.com>
2022-03-04 19:25:39 +0300

    dinamik veri

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -6,8 +6,8 @@ from . import views
 # http://127.0.0.1:8000/blogs/3     => blog-details
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="home"),
     path("index", views.index),
-    path("blogs", views.blogs),
-    path("blogs/<int:id>", views.blog_details),
+    path("blogs", views.blogs, name="blogs"),
+    path("blogs/<int:id>", views.blog_details, name="blog_details"),
 ]
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,14 +1,58 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+data = {
+    "blogs": [
+        {
+            "id": 1,
+            "title": "komple web geliştirme",
+            "image": "1.jpeg",
+            "is_active": True,
+            "is_home": False,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 2,
+            "title": "python kursu",
+            "image": "2.jpeg",
+            "is_active": True,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 3,
+            "title": "django kursu",
+            "image": "3.jpeg",
+            "is_active": False,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        }
+    ]
+}
+
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/index.html", context)
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/blogs.html", context)
 
 def blog_details(request, id):
+    # blogs = data["blogs"]
+    # selected_blogs = None
+
+    # for blog in blogs:
+    #     if blog["id"] == id:
+    #         selected_blogs = blog
+    blogs = data["blogs"]
+
+    selectedBlog = [blog for blog in blogs if blog["id"]==id][0]
     return render(request, "blog/blog-details.html", {
-        "id": id
+        "blog": selectedBlog
     })
